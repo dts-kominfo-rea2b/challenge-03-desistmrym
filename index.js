2b6001e723c79668e647026b8f70c89c77ab2754
desistmrym <desisitimaryam@gmail.com>
2022-06-02 09:36:53 +0700

    add challenge function

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = function(data) {
+  const all_data = [];
+  data.forEach(item => {
+    all_data.push(`- ${item.nama} x${item.kuantitas}`)
+  })
+  return all_data;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = function(data) {
+  let total = 0;
+  data.forEach(item => {
+    total += item.harga
+  })
+  return total;
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
